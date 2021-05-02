4be1a63edc4eb05d8db56e2e2a5734c0374c3490
Priyadharshini0101 <83385932+Priyadharshini0101@users.noreply.github.com>
2021-05-02 13:54:12 +0530

    Update

--- login and register/username.php
+++ login and register/username.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['project'])){
 if(isset($_POST['submit'])){
     $email=$_POST['email'];
     $password=md5($_POST['password']);
-    $sql="SELECT * FRoM users WHERE email='$email' AND password='$password'";
+    $sql="SELECT * FROM users WHERE email='$email' AND password='$password'";
     $result=mysqli_query($conn,$sql);
     if($result->num_rows>0){
         $row=mysqli_fetch_assoc($result);
